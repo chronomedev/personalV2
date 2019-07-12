de3aefb2c278d0fe9fa615a32efaf2193196b491
Hansrenee Willysandro <hansrenee@live.com>
2019-07-12 23:58:15 +0700

    Updated Responsive layout
    More efficient Javascript Code tweak

--- plugin/kelasDOM.js
+++ plugin/kelasDOM.js
@@ -3,21 +3,20 @@
 // 2019 Hansrenee Willysandro / HSD
 
 var kelasDOM = new function(){
-    this.bagian1Show = function(){
-        document.getElementById('resp1').innerHTML = "<center><div class='center_content'><h1>Hansrenee Development System</h1><h4>Welcome to the official site of Hansrenee</h4></div></center>"
-    };
+    // this.bagian1Show = function(){
+    //     document.getElementById('resp1').innerHTML = "<center><div class='center_content'><h1>Hansrenee Development System</h1><h4>Welcome to the official site of Hansrenee</h4></div></center>"
+    // };
 
     this.bagian1Hide = function(){
         document.getElementById('resp1').innerHTML = "";
         //console.log("woi hideeee");
     };
 
-    this.bagian2Show = function(){
-
+    this.writeDOM = function(elemen, isi){
+        document.getElementById(elemen).innerHTML = isi;
     };
 
-    this.bagian3Hide = function(){
 
-    };
+
 
 }
--- plugin/responsif.js
+++ plugin/responsif.js
@@ -2,14 +2,24 @@
 // Script that handles the websites HTML DOM elements
 //2019 hsd 
 $(document).ready(function() {
+    var dom_baris1 = document.getElementById('uno').innerHTML;
+    var dom_barisduo = document.getElementById('duo').innerHTML;
+    var dom_baris2 = document.getElementById('resp2').innerHTML;
+    var dom_baris3 = document.getElementById('resp3').innerHTML;
+    console.log(dom_baris2);
     $(window).on('resize', function() {
-        if ($(window).width() < 750) {
-            $('#eins').hide();
-            kelasDOM.bagian1Show();    
+        if ($(window).width() < 585) {
+            // $('#eins').hide();
+            // kelasDOM.bagian1Show();
+            kelasDOM.writeDOM('uno', dom_barisduo);
+            kelasDOM.writeDOM('duo', dom_baris1);
+            kelasDOM.writeDOM('resp2', dom_baris3);
+            kelasDOM.writeDOM('resp3',dom_baris2);
         } else {
-            kelasDOM.bagian1Hide();
-            $('#eins').show();
-
+            kelasDOM.writeDOM('uno', dom_baris1);
+            kelasDOM.writeDOM('duo', dom_barisduo);
+            kelasDOM.writeDOM('resp2', dom_baris2);
+            kelasDOM.writeDOM('resp3',dom_baris3);
         }
     });
 });
