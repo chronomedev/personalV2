a37e1b9e927a3ee9f3697734a21cef226463a33b
Hansrenee Willysandro <hansrenee>
2019-07-17 18:14:01 +0700

    Added game fundamental Function to repository

---
+++ game/main.js
@@ -0,0 +1,26 @@
+//Main game script
+// Based on Indonesian popular Tebak lima dasar Games
+// 2019 HSD
+
+pemain;
+skor;
+
+function createPlayer(list_player){
+    for(z = 0;z<list_player.Length;z++){
+        pemain[z] = list_player[z];
+        skor[z] = 0;
+    }
+
+}
+
+
+function createRoom(){
+
+}
+
+
+function createGameSession(){
+
+}
+
+
